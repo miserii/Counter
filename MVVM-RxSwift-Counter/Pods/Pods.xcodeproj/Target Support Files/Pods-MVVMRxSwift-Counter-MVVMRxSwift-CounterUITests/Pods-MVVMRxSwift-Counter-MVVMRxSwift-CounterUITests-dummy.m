b461d2c667aa680e375be695169555c50988dda1
Osaka Miseri <59869820+miserii@users.noreply.github.com>
2021-01-01 17:26:04 +0900

    Delete Pods-MVVMRxSwift-Counter-MVVMRxSwift-CounterUITests-dummy.m

--- MVVM-RxSwift-Counter/Pods/Pods.xcodeproj/Target Support Files/Pods-MVVMRxSwift-Counter-MVVMRxSwift-CounterUITests/Pods-MVVMRxSwift-Counter-MVVMRxSwift-CounterUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MVVMRxSwift_Counter_MVVMRxSwift_CounterUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MVVMRxSwift_Counter_MVVMRxSwift_CounterUITests
-@end
